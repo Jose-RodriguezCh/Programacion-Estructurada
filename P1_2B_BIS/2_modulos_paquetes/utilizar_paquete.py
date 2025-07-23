93c4245fc820a988ddc8724a358fbf384b866c69
Jose <jose_3141240158@utd.edu.mx>
2025-07-23 17:03:45 -0600

    Delete P1_2B_BIS directory

--- P1_2B_BIS/2_modulos_paquetes/utilizar_paquete.py
+++
@@ -1,8 +0,0 @@
-from paquete1 import modulos
-
-print(modulos.saludar("Daniel C"))
-
-modulos.borrarPantalla()
-nom,tel=modulos.solicitarDatos2()
-print(f"\n\t.::Agenda Teledonica::.\n\tNombre: {nom}\n\tTelefono: {tel}")
-modulos.esperaTecla()
